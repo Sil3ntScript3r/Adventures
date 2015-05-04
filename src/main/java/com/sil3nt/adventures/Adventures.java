72198fdf9737b39d4e6e62fffa529474db90e514
Sil3nt <sil3ntscript3r@gmail.com>
2015-05-04 17:05:53 -0400

    Initial commit

---
+++ src/main/java/com/sil3nt/adventures/Adventures.java
@@ -0,0 +1,30 @@
+package com.sil3nt.adventures;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="Adventures", name="Adventures", version="0.0.0")
+public class Adventures {
+    @Mod.Instance("Adventures")
+    public static Adventures instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
